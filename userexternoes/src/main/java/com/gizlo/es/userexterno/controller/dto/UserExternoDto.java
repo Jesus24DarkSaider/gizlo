20a08cc1e03f8b2e9e8b45808ecdb2f3361663e5
jesus <carlvall8520@gmail.com>
2021-10-19 14:11:25 -0500

    se agrega dto

---
+++ userexternoes/src/main/java/com/gizlo/es/userexterno/controller/dto/UserExternoDto.java
@@ -0,0 +1,98 @@
+package com.gizlo.es.userexterno.controller.dto;
+
+import java.util.UUID;
+
+public class UserExternoDto {
+
+	private UUID id;
+	private String nombre;
+	private String apellidos;
+	private String correoPersonal;
+	private String ciudad;
+	private int edad;
+
+	/**
+	 * @return the id
+	 */
+	public UUID getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/**
+	 * @return the apellidos
+	 */
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	/**
+	 * @param apellidos the apellidos to set
+	 */
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	/**
+	 * @return the correoPersonal
+	 */
+	public String getCorreoPersonal() {
+		return correoPersonal;
+	}
+
+	/**
+	 * @param correoPersonal the correoPersonal to set
+	 */
+	public void setCorreoPersonal(String correoPersonal) {
+		this.correoPersonal = correoPersonal;
+	}
+
+	/**
+	 * @return the ciudad
+	 */
+	public String getCiudad() {
+		return ciudad;
+	}
+
+	/**
+	 * @param ciudad the ciudad to set
+	 */
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+
+	/**
+	 * @return the edad
+	 */
+	public int getEdad() {
+		return edad;
+	}
+
+	/**
+	 * @param edad the edad to set
+	 */
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+}
