be34af146a03d705265bef7492e143e4b1105d47
JESUS\JESUS <carlvall8520@gmail.com>
2021-10-19 13:13:54 -0500

    usuario externo

---
+++ userexternoes/src/main/java/com/gizlo/es/userexterno/UserexternoesApplication.java
@@ -0,0 +1,13 @@
+package com.gizlo.es.userexterno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserexternoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserexternoesApplication.class, args);
+	}
+
+}
---
+++ userexternoes/src/test/java/com/gizlo/es/userexterno/UserexternoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gizlo.es.userexterno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserexternoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
